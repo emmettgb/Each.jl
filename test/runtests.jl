5c6df471676bc4f5bb3462762d98e9d481bc04a0
David Gustavsson <david.e.gustavsson@gmail.com>
2020-07-26 17:23:20 +0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using Each
 using Test
 
 @testset "Each.jl" begin
-    # Write your tests here.
+    @test sum.(each(Tuple{Vararg{Number}},((1,1),(1,2),(1,3)))) == (2,3,4)
+    @test sum.(each(Tuple{Vararg{Number}},(1,1))) == (2,)
+    @test_throws ErrorException each(String,((1,2),(3,4)))
+    @test eltype(each(String,"something")) == String
 end
