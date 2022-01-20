b2c8df9b1af171d3ff8ed8b1f2ea30a36f91f94e
Em <52672675+emmettgb@users.noreply.github.com>
2022-01-19 23:15:15 -0500

    added macro @each to tests, made test-set for each()
    Ideally, this whole thing would be wrapped in a src/ repositiory, but either way -- boom, tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,18 @@ using Each
 using Test
 
 @testset "Each.jl" begin
-    @test sum.(each(Tuple{Vararg{Number}},((1,1),(1,2),(1,3)))) == (2,3,4)
-    @test sum.(each(Tuple{Vararg{Number}},(1,1))) == (2,)
-    @test_throws ErrorException each(String,((1,2),(3,4)))
-    @test eltype(each(String,"something")) == String
+    @testset "each()" begin
+        @test sum.(each(Tuple{Vararg{Number}},((1,1),(1,2),(1,3)))) == (2,3,4)
+        @test sum.(each(Tuple{Vararg{Number}},(1,1))) == (2,)
+        @test_throws ErrorException each(String,((1,2),(3,4)))
+        @test eltype(each(String,"something")) == String
+    end
+    @testset "@each" begin
+        x = [5, 10, 15, 20]
+        @test @each x > 0 == true
+        @test @each x < 0 == false
+        @test @each x % 5 == true
+        @test @each x == 5 == false
+    end
+    
 end
